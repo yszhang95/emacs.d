408944ca1d7adc20b2b4de99e81b7f76ac7cede4
Yousen Zhang <zysright@gmail.com>
2022-08-18 16:12:09 +0200

    add anki-editor and pyim

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,5 +2,100 @@
 ;;; Commentary:
 ;;; Code:
 
+;;; copy from https://yiufung.net/post/anki-org/
+(use-package anki-editor
+  :after org
+  :bind (:map org-mode-map
+              ("<f12>" . anki-editor-cloze-region-auto-incr)
+              ("<f11>" . anki-editor-cloze-region-dont-incr)
+              ("<f10>" . anki-editor-reset-cloze-number)
+              ("<f9>"  . anki-editor-push-tree))
+  :hook (org-capture-after-finalize . anki-editor-reset-cloze-number) ; Reset cloze-number after each capture.
+  :config
+  (setq anki-editor-create-decks t ;; Allow anki-editor to create a new deck if it doesn't exist
+        anki-editor-org-tags-as-anki-tags t)
+
+  (defun anki-editor-cloze-region-auto-incr (&optional arg)
+    "Cloze region without hint and increase card number."
+    (interactive)
+    (anki-editor-cloze-region my-anki-editor-cloze-number "")
+    (setq my-anki-editor-cloze-number (1+ my-anki-editor-cloze-number))
+    (forward-sexp))
+  (defun anki-editor-cloze-region-dont-incr (&optional arg)
+    "Cloze region without hint using the previous card number."
+    (interactive)
+    (anki-editor-cloze-region (1- my-anki-editor-cloze-number) "")
+    (forward-sexp))
+  (defun anki-editor-reset-cloze-number (&optional arg)
+    "Reset cloze number to ARG or 1"
+    (interactive)
+    (setq my-anki-editor-cloze-number (or arg 1)))
+  (defun anki-editor-push-tree ()
+    "Push all notes under a tree."
+    (interactive)
+    (anki-editor-push-notes '(4))
+    (anki-editor-reset-cloze-number))
+  ;; Initialize
+  (anki-editor-reset-cloze-number)
+  )
+
+;; Org-capture templates
+(setq org-my-anki-file "/Users/yousen/anki.org")
+(add-to-list 'org-capture-templates
+             '("a" "Anki basic"
+               entry
+               (file+headline org-my-anki-file "Dispatch Shelf")
+               "* %<%H:%M>   %^g\n:PROPERTIES:\n:ANKI_NOTE_TYPE: Basic\n:ANKI_DECK: Mega\n:END:\n** Front\n%?\n** Back\n%x\n"))
+(add-to-list 'org-capture-templates
+             '("A" "Anki cloze"
+               entry
+               (file+headline org-my-anki-file "Dispatch Shelf")
+               "* %<%H:%M>   %^g\n:PROPERTIES:\n:ANKI_NOTE_TYPE: Cloze\n:ANKI_DECK: Mega\n:END:\n** Text\n%x\n** Extra\n"))
+
+;;; Chinese input from https://github.com/tumashu/pyim
+(require 'pyim)
+(require 'pyim-basedict)
+(require 'pyim-cregexp-utils)
+(pyim-basedict-enable)
+
+(setq default-input-method "pyim")
+
+;; 显示5个候选词。
+(setq pyim-page-length 5)
+
+;; 金手指设置，可以将光标处的编码，比如：拼音字符串，转换为中文。
+(global-set-key (kbd "M-j") 'pyim-convert-string-at-point)
+
+;; 按 "C-<return>" 将光标前的 regexp 转换为可以搜索中文的 regexp.
+(define-key minibuffer-local-map (kbd "C-<return>") 'pyim-cregexp-convert-at-point)
+
+;; 我使用全拼
+(pyim-default-scheme 'quanpin)
+;; (pyim-default-scheme 'wubi)
+;; (pyim-default-scheme 'cangjie)
+
+;; 我使用云拼音
+;; (setq pyim-cloudim 'baidu)
+(setq pyim-cloudim 'google)
+
+;; pyim 探针设置
+;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
+;; 我自己使用的中英文动态切换规则是：
+;; 1. 光标只有在注释里面时，才可以输入中文。
+;; 2. 光标前是汉字字符时，才能输入中文。
+;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
+;; (setq-default pyim-english-input-switch-functions
+;;               '(pyim-probe-dynamic-english
+;;                 pyim-probe-isearch-mode
+;;                 pyim-probe-program-mode
+;;                 pyim-probe-org-structure-template))
+
+;; (setq-default pyim-punctuation-half-width-functions
+;;               '(pyim-probe-punctuation-line-beginning
+;;                 pyim-probe-punctuation-after-punctuation))
+
+;; 开启代码搜索中文功能（比如拼音，五笔码等）
+;; (pyim-isearch-mode 1)
+
 (provide 'init-local)
 ;;; init-local.el ends here
