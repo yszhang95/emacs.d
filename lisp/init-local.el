dfa960b54052371641b15b23dafd3d049bcf2682
Yousen Zhang <zysright@gmail.com>
2022-09-10 23:26:21 +0200

    added windows font

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -6,10 +6,17 @@
 ;;; msys64 has been installed
 (if (eq system-type 'windows-nt)
     ;;; ispell
-    (setq ispell-program-name "C:\\msys64\\mingw64\\bin\\aspell")
+    (progn
+      ;;; ispell using msys64
+      (setq ispell-program-name "C:\\msys64\\mingw64\\bin\\aspell")
+      ;;; font face under resolution 1920x1080
+      ;;; https://github.com/adobe-fonts/source-han-mono/releases
+      ;;; https://www.emacswiki.org/emacs/ChangeFontsPermanentlyOnWindows
+      (set-face-attribute 'default nil :family "Source Han Mono" :height 100)
+      )
   )
 
-;;; mac special face of font 
+;;; mac special face of font
 (if (eq system-type 'darwin)
   ;;; https://osxdaily.com/2018/01/07/use-sf-mono-font-mac/
     (set-face-attribute 'default nil :font "SF Mono-12")
