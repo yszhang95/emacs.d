511ea77d610a5d901e350453fcffe2c079b94c4e
Yousen Zhang <zysright@gmail.com>
2022-09-10 17:35:00 +0200

    face of font on mac

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -8,9 +8,12 @@
     ;;; ispell
     (setq ispell-program-name "C:\\msys64\\mingw64\\bin\\aspell")
   )
-;;; I have issue of display for EmacsMac and Emacs WindowsNT
-;;; override the vertical line for column indicator
-(setq-default display-fill-column-indicator-character ?\u007c)
+
+;;; mac special face of font 
+(if (eq system-type 'darwin)
+  ;;; https://osxdaily.com/2018/01/07/use-sf-mono-font-mac/
+    (set-face-attribute 'default nil :font "SF Mono-12")
+)
 
 ;;; anki-editor
 (require 'init-anki)
